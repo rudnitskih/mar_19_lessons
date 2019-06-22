3eb4a88d905c9f1c87ce332d09e91c5a0a1544f3
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2019-06-22 10:30:04 +0300

    34 lesson snippet - add router

--- lesson_32_react/src/App.jsx
+++ lesson_32_react/src/App.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import './App.css';
 import {Header} from "./Header/Header";
 import {StoreCard} from "./StoreCard/StoreCard";
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
 
 
-function App() {
+function RestaurantPage() {
+  return <h1>
+    Musafitr Restaurant page
+    <Link to="/">Back to main</Link>
+  </h1>;
+}
+
+function MainPage() {
   const restaurants = [
     {
       title: 'Musafir',
@@ -21,28 +29,36 @@ function App() {
   ];
 
   return (
+    <ul className="app__restaurants">
+      {restaurants.map((restaurent, i) => {
+        return (
+          <li key={i}>
+            <Link to="/restaurant">
+              <StoreCard
+                restaurent={restaurent}
+              />
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+function App() {
+  return (
     <>
       <Header />
-      <ul className="app__restaurants">
-        {restaurants.map(({title, categories}, i) => {
-          return (
-            <li key={i}>
-              <StoreCard
-                title={title}
-                categories={categories}
-              />
-            </li>
-          )
-        })}
-      </ul>
+
+      <Router>
+        <Route path="/"
+               exact
+               component={MainPage}/>
+        <Route path="/restaurant"
+               component={RestaurantPage}/>
+      </Router>
     </>
   );
-
-  // return (
-  //   <div className="App">
-  //     <Header />
-  //   </div>
-  // );
 }
 
 export default App;
--- lesson_32_react/src/StoreCard/StoreCard.jsx
+++ lesson_32_react/src/StoreCard/StoreCard.jsx
@@ -4,9 +4,9 @@ import './StoreCard.css'
 export function StoreCard(props) {
   return (
     <div className="store-card">
-      <span className="store-card__title">{props.title}</span>
+      <span className="store-card__title">{props.restaurent.title}</span>
       <ul className="store-card__categories">
-        {props.categories.map((category, i) => {
+        {props.restaurent.categories.map((category, i) => {
           return (
             <li className="store-card__category" key={i}>{category}</li>
           )
