51ed7d58048f6837209865d369cc979ede9e4cdc
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2019-07-01 19:52:01 +0300

    Add var

--- lesson_32_react/src/MainPage/MainPage.jsx
+++ lesson_32_react/src/MainPage/MainPage.jsx
@@ -3,13 +3,15 @@ import { Search } from "../Search/Search";
 import { Restaraunts } from "../Restaraunts/Restaraunts";
 
 export function MainPage() {
+  const title = "Kyiv Restararunt";
+
   return (
     <div className="main-page">
       <div className="main-page__search">
         <Search />
       </div>
 
-      <h1 className="main-page__title">Kyiv Restararunt</h1>
+      <h1 className="main-page__title">{title}</h1>
 
       <Restaraunts />
     </div>
